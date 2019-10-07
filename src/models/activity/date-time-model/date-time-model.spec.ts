09e4612f4c2752000d0da3311daa76a42230c787
vitalii.toderian <vitalii.toderian@gmail.com>
2019-10-07 12:35:03 +0300

    beta date-time picker added/ Intention to remove it

---
+++ src/models/activity/date-time-model/date-time-model.spec.ts
@@ -0,0 +1,7 @@
+import { DateTimeModel } from './date-time-model';
+
+describe('DateTimeModel', () => {
+  it('should create an instance', () => {
+    expect(new DateTimeModel()).toBeTruthy();
+  });
+});
