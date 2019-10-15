2179f7f5cae564c0e4d7e6554b86e128660eb861
vitalii.toderian <vitalii.toderian@gmail.com>
2019-10-15 23:56:28 +0300

    before installing npm install ngx-daterangepicker-material --save

--- src/models/activity/date-time-model/date-time-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DateTimeModel } from './date-time-model';
-
-describe('DateTimeModel', () => {
-  it('should create an instance', () => {
-    expect(new DateTimeModel()).toBeTruthy();
-  });
-});
